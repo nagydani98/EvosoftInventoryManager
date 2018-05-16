b5b5eb3b379f6a5c9b0839f682f17a3ba1849c9a
rg <rg@192.168.0.164>
2018-05-16 16:17:36 +0200

    created Item package, some barebones terminal in main

--- evo/src/Terminal/mainterminal.java
+++ evo/src/Terminal/mainterminal.java
@@ -1,10 +1,22 @@
 package Terminal;
 
-public class mainterminal {
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.beryx.textio.TextTerminal;
 
+public class mainterminal {
+//main terminal, ezt nem kell szerintem kiszervezni külön classba
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TextIO textIO = TextIoFactory.getTextIO();
+		TextTerminal terminal = textIO.getTextTerminal();
+		terminal.println("Indul");
+		
+		terminal.dispose();
+		textIO.dispose();
+		
 	}
 
 }

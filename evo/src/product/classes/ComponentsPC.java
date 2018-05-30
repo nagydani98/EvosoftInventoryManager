ae403b627e2f209ed0c5534894cdf77dff70aa73
Megyeri László <36531936+Mlaszlo95@users.noreply.github.com>
2018-05-30 17:17:58 +0200

    Delete ComponentsPC.java

--- evo/src/product/classes/ComponentsPC.java
+++
@@ -1,8 +0,0 @@
-package product.classes;
-
-public class ComponentsPC extends Products{
-	public ComponentsPC(String name,String producer,int quantity, int price) {
-		super(name,producer,quantity,price);
-		setCategorical("ComponentsPC");
-	}
-}
